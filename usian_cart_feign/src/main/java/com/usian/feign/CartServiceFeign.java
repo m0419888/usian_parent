22c101d21d1ebf4e0c3e007a448f1a907ffc08f5
m0419888 <1105847187@qq.com>
2020-06-11 20:01:15 +0800

    更新-购物车功能部分

---
+++ usian_cart_feign/src/main/java/com/usian/feign/CartServiceFeign.java
@@ -0,0 +1,18 @@
+package com.usian.feign;
+
+import com.usian.pojo.TbItem;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@FeignClient("usian-cart-service")
+public interface CartServiceFeign {
+
+    @RequestMapping("/service/cart/selectCartByUserId")
+    Map<String, TbItem> selectCartByUserId(@RequestParam String userId);
+
+    @RequestMapping("/service/cart/insertCart?userId=234234")
+    Boolean insertCart(@RequestParam String userId, Map<String, TbItem> cart);
+}
